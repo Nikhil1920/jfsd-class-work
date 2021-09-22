3661adb5bc386094876fabb76c910fa2ac2daeeb
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-09-22 21:14:39 +0530

    add skill 7 code

--- skill/skill-6/src/skill6/skill6.java
+++ skill/skill-6/src/skill6/skill6.java
@@ -29,7 +29,11 @@ public class skill6 {
 			System.out.println("1. Get employee details");
 			System.out.println("2. Update employee salary");
 			System.out.println("3. Delete employee with least salary");
-			System.out.println("4. Exit the program");
+			System.out.println("4. Show all employees details");
+			System.out.println("5. Show employees greater than this age");
+			System.out.println("6. Delete a employee");
+			System.out.println("7. update employee name");
+			System.out.println("8. Exit the program");
 
 			int choice = sc.nextInt();
 
@@ -51,6 +55,31 @@ public class skill6 {
 					break;
 
 				case 4:
+					get_all_employees_details();
+					break;
+
+				case 5:
+					System.out.println("Please enter minimum age");
+					int minage = sc.nextInt();
+					get_employees_above_age(minage);
+					break;
+
+				case 6:
+					System.out.println("Enter the id of employee to be deleted");
+					int empid = sc.nextInt();
+					delete_a_employee(empid);
+					break;
+
+				case 7:
+					System.out.println("Enter the id of employee to be renamed");
+					int emp_id = sc.nextInt();
+					sc.nextLine();
+					System.out.println("Enter new name");
+					String new_name = sc.nextLine();
+					update_employee_name(emp_id, new_name);
+					break;
+
+				case 8:
 					exit = true;
 					break;
 
@@ -91,6 +120,7 @@ public class skill6 {
 
 	public static void update_employee_details(int Empid) {
 		try (Session session = getSession()) {
+			@SuppressWarnings("unchecked")
 			List<Double> average = session.createQuery("select avg(salary) from Employee").getResultList();
 			Transaction tx = session.beginTransaction();
 			Integer avg = average.get(0).intValue();
@@ -107,6 +137,7 @@ public class skill6 {
 
 	public static void delete_minimum_salary_employees() {
 		try (Session session = getSession()) {
+			@SuppressWarnings("unchecked")
 			List<Integer> minimum = session.createQuery("select min(salary) from Employee").getResultList();
 			Transaction tx = session.beginTransaction();
 			Integer min = minimum.get(0);
@@ -121,4 +152,81 @@ public class skill6 {
 		}
 	}
 
+	public static void get_all_employees_details() {
+		try (Session session = getSession()) {
+
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+
+			CriteriaQuery<Employee> criteria = builder.createQuery(Employee.class);
+			Root<Employee> employeeRoot = criteria.from(Employee.class);
+			criteria.select(employeeRoot);
+
+			List<Employee> employeeList = session.createQuery(criteria).getResultList();
+			if (employeeList.isEmpty()) {
+				System.out.println("No employee found");
+			} else {
+				System.out.format("%10s%14s%14s%10s%10s\n", "Empid", "Emp Name", "Department", "Age", "Salary");
+				for (Employee temp : employeeList) {
+					System.out.format("%10d%14s%14s%10d%10d\n", temp.getEmpid(), temp.getEname(), temp.getDepartment(),
+							temp.getAge(), temp.getSalary());
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Something went wrong");
+		}
+	}
+
+	public static void get_employees_above_age(int empage) {
+		try (Session session = getSession()) {
+
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+
+			CriteriaQuery<Employee> criteria = builder.createQuery(Employee.class);
+			Root<Employee> employeeRoot = criteria.from(Employee.class);
+			criteria.select(employeeRoot);
+
+			criteria.where(builder.greaterThan(employeeRoot.get("age"), empage));
+			List<Employee> employeeList = session.createQuery(criteria).getResultList();
+			if (employeeList.isEmpty()) {
+				System.out.println("No employee found");
+			} else {
+				System.out.println("Details of employees with age greater than " + empage + " are");
+				System.out.format("%10s%14s%14s%10s%10s\n", "Empid", "Emp Name", "Department", "Age", "Salary");
+				for (Employee temp : employeeList) {
+					System.out.format("%10d%14s%14s%10d%10d\n", temp.getEmpid(), temp.getEname(), temp.getDepartment(),
+							temp.getAge(), temp.getSalary());
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Something went wrong");
+		}
+	}
+
+	public static void delete_a_employee(int empid) {
+		try (Session session = getSession()) {
+			Transaction tx = session.beginTransaction();
+			int updatedEntities = session.createQuery("delete from Employee e where e.Empid=:i")
+					.setParameter("i", empid).executeUpdate();
+
+			System.out.println(updatedEntities + " Entities deleted succesfully");
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("Something went wrong");
+		}
+	}
+
+	public static void update_employee_name(int emp_id, String new_name) {
+		try (Session session = getSession()) {
+			Transaction tx = session.beginTransaction();
+			int updatedEntities = session.createQuery("update Employee e set e.Ename=:n where e.Empid=:i")
+					.setParameter("n", new_name).setParameter("i", emp_id).executeUpdate();
+
+			System.out.println(updatedEntities + " Employee details updated");
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("Something went wrong");
+		}
+	}
 }
